8d17e76405d89ed123ab9f67beac406a0607f39b
aewebb80 <tug41380@temple.edu>
2017-03-02 15:45:23 -0500

    Testing paths

--- andrew/vcf_tests.py
+++ andrew/vcf_tests.py
@@ -12,8 +12,8 @@ class vcftools_tests (unittest.TestCase):
     
     def test_produce_vcftools_log (self):
         vcf_scripts.produce_vcftools_log('Log Test:\n1\n2\n3\n', 'out', '.logTest')
-        self.assertTrue(compare_to_expected('andrew' + '/' + 'out.logTest.log', 'andrew' + '/' + 'example/locus8.logTest.log'))
-        self.addCleanup(os.remove, 'andrew' + '/' + 'out.logTest.log')
+        self.assertTrue(compare_to_expected('out.logTest.log', 'andrew' + '/' + 'example/locus8.logTest.log'))
+        self.addCleanup(os.remove, 'out.logTest.log')
     
     def test_return_basic_args (self):
         vcf_args = vcf_scripts.vcf_argument_parser(['--gzvcf', 'andrew' + '/' + 'example/locus8.vcf.gz'])
@@ -27,33 +27,33 @@ class vcftools_tests (unittest.TestCase):
 
     def test_calculate_Fst (self):
         vcf_scripts.calculate_Fst(['--gzvcf', 'andrew' + '/' + 'example/locus8.vcf.gz', '--weir-fst-pops', 'andrew' + '/' + 'example/Paniscus.txt', 'andrew' + '/' + 'example/Troglodytes.txt', '--out', 'out'])
-        self.assertTrue(compare_to_expected('andrew' + '/' + 'out.windowed.weir.fst', 'andrew' + '/' + 'example/locus8.windowed.weir.fst'))
-        self.addCleanup(os.remove, 'andrew' + '/' + 'out.windowed.weir.fst')
-        self.addCleanup(os.remove, 'andrew' + '/' + 'out.windowed.weir.fst.log')
+        self.assertTrue(compare_to_expected('out.windowed.weir.fst', 'andrew' + '/' + 'example/locus8.windowed.weir.fst'))
+        self.addCleanup(os.remove, 'out.windowed.weir.fst')
+        self.addCleanup(os.remove, 'out.windowed.weir.fst.log')
     
     def test_calculate_tajimasD (self): 
         vcf_scripts.calculate_tajimasD(['--gzvcf', 'andrew' + '/' + 'example/locus8.vcf.gz', '--out', 'out'])
         self.assertTrue(compare_to_expected('out.Tajima.D', 'andrew' + '/' + 'example/locus8.Tajima.D'))
-        self.addCleanup(os.remove, 'andrew' + '/' + 'out.Tajima.D')
-        self.addCleanup(os.remove, 'andrew' + '/' + 'out.Tajima.D.log')
+        self.addCleanup(os.remove, 'out.Tajima.D')
+        self.addCleanup(os.remove, 'out.Tajima.D.log')
     
     def test_calculate_pi (self): 
         vcf_scripts.calculate_pi(['--gzvcf', 'example/locus8.vcf.gz', '--out', 'out'])
-        self.assertTrue(compare_to_expected('andrew' + '/' + 'out.windowed.pi', 'andrew' + '/' + 'example/locus8.windowed.pi'))
-        self.addCleanup(os.remove, 'andrew' + '/' + 'out.windowed.pi')
-        self.addCleanup(os.remove, 'andrew' + '/' + 'out.windowed.pi.log')
+        self.assertTrue(compare_to_expected('out.windowed.pi', 'andrew' + '/' + 'example/locus8.windowed.pi'))
+        self.addCleanup(os.remove, 'out.windowed.pi')
+        self.addCleanup(os.remove, 'out.windowed.pi.log')
     
     def test_calculate_af(self):    
         vcf_scripts.calculate_af(['--gzvcf', 'andrew' + '/' + 'example/locus8.vcf.gz', '--out', 'out'])
-        self.assertTrue(compare_to_expected('andrew' + '/' + 'out.frq', 'andrew' + '/' + 'example/locus8.frq'))
-        self.addCleanup(os.remove, 'andrew' + '/' + 'out.frq')
-        self.addCleanup(os.remove, 'andrew' + '/' + 'out.frq.log')
+        self.assertTrue(compare_to_expected('out.frq', 'andrew' + '/' + 'example/locus8.frq'))
+        self.addCleanup(os.remove, 'out.frq')
+        self.addCleanup(os.remove, 'out.frq.log')
     
     def test_calculate_het(self):    
         vcf_scripts.calculate_heterozygosity(['--gzvcf', 'andrew' + '/' + 'example/locus8.vcf.gz', '--out', 'out'])
-        self.assertTrue(compare_to_expected('andrew' + '/' + 'out.het', 'andrew' + '/' + 'example/locus8.het'))
-        self.addCleanup(os.remove, 'andrew' + '/' + 'out.het')
-        self.addCleanup(os.remove, 'andrew' + '/' + 'out.het.log')
+        self.assertTrue(compare_to_expected('out.het', 'andrew' + '/' + 'example/locus8.het'))
+        self.addCleanup(os.remove, 'out.het')
+        self.addCleanup(os.remove, 'out.het.log')
 
 if __name__ == "__main__":
     unittest.main()
