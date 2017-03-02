59a6bfd1385f2f5535fc307c00c2ac7f6da06eaf
aewebb80 <tug41380@temple.edu>
2017-03-02 17:44:09 -0500

    Testing installing vcftools from source

--- install_vcftools.sh
+++ install_vcftools.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 set -ex
 wget https://github.com/vcftools/vcftools/tarball/master -O vcftools.tar.gz
-tar -xzvf vcftools.tar.gz
-cd vcftools && ./configure --prefix=/usr && make && sudo make install
