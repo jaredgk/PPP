b76cf41779664fc4fda908c5b9e2fb1dfeb6b0b3
aewebb80 <tug41380@temple.edu>
2017-03-10 17:32:20 -0500

    Updated vcf_scripts to a general wrapper. Also updated vcf_tests to reflect these changes

--- andrew/vcf_tests.py
+++ andrew/vcf_tests.py
@@ -14,43 +14,45 @@ class vcftools_tests (unittest.TestCase):
         vcf_scripts.produce_vcftools_log('Log Test:\n1\n2\n3\n', 'out', '.logTest')
         self.assertTrue(compare_to_expected('out.logTest.log', 'example/locus8.logTest.log'))
         self.addCleanup(os.remove, 'out.logTest.log')
-    
-    def test_return_basic_args (self):
-        vcf_args = vcf_scripts.vcf_argument_parser(['--gzvcf', 'example/locus8.vcf.gz'])
-        self.assertEqual(vcf_scripts.return_basic_args(vcf_args), ['vcftools', '--gzvcf', 'example/locus8.vcf.gz', '--out', 'out'])
-    
+        
     def test_check_vcftools_for_errors (self):
         self.assertTrue(vcf_scripts.check_vcftools_for_errors('Run Time'))
         with self.assertRaises(SystemExit) as cm:
             vcf_scripts.check_vcftools_for_errors('SegFault')
         self.assertEqual(cm.exception.code, 'Error with vcftools')
 
-    def test_calculate_Fst (self):
-        vcf_scripts.calculate_Fst(['--gzvcf', 'example/locus8.vcf.gz', '--weir-fst-pops', 'example/Paniscus.txt', 'example/Troglodytes.txt', '--out', 'out'])
+    def test_Fst (self):
+        vcf_scripts.vcftools_wrapper(['--gzvcf', 'example/locus8.vcf.gz', '--weir-fst-pop', 'example/Paniscus.txt', '--weir-fst-pop', 'example/Troglodytes.txt', '--out', 'out'])
+        self.assertTrue(compare_to_expected('out.weir.fst', 'example/locus8.weir.fst'))
+        self.addCleanup(os.remove, 'out.weir.fst')
+        self.addCleanup(os.remove, 'out.weir.fst.log')
+    
+    def test_Fst_window (self):
+        vcf_scripts.vcftools_wrapper(['--gzvcf', 'example/locus8.vcf.gz', '--weir-fst-pop', 'example/Paniscus.txt', '--weir-fst-pop', 'example/Troglodytes.txt', '--out', 'out', '--fst-window-size', '--fst-window-step'])
         self.assertTrue(compare_to_expected('out.windowed.weir.fst', 'example/locus8.windowed.weir.fst'))
         self.addCleanup(os.remove, 'out.windowed.weir.fst')
         self.addCleanup(os.remove, 'out.windowed.weir.fst.log')
-    
-    def test_calculate_tajimasD (self): 
-        vcf_scripts.calculate_tajimasD(['--gzvcf', 'example/locus8.vcf.gz', '--out', 'out'])
+
+    def test_tajimasD (self): 
+        vcf_scripts.vcftools_wrapper(['--gzvcf', 'example/locus8.vcf.gz', '--out', 'out', '--TajimaD'])
         self.assertTrue(compare_to_expected('out.Tajima.D', 'example/locus8.Tajima.D'))
         self.addCleanup(os.remove, 'out.Tajima.D')
         self.addCleanup(os.remove, 'out.Tajima.D.log')
-    
-    def test_calculate_pi (self): 
-        vcf_scripts.calculate_pi(['--gzvcf', 'example/locus8.vcf.gz', '--out', 'out'])
+        
+    def test_window_pi (self): 
+        vcf_scripts.vcftools_wrapper(['--gzvcf', 'example/locus8.vcf.gz', '--out', 'out', '--window-pi'])
         self.assertTrue(compare_to_expected('out.windowed.pi', 'example/locus8.windowed.pi'))
         self.addCleanup(os.remove, 'out.windowed.pi')
         self.addCleanup(os.remove, 'out.windowed.pi.log')
-    
-    def test_calculate_af(self):    
-        vcf_scripts.calculate_af(['--gzvcf', 'example/locus8.vcf.gz', '--out', 'out'])
+        
+    def test_freq (self):    
+        vcf_scripts.vcftools_wrapper(['--gzvcf', 'example/locus8.vcf.gz', '--out', 'out', '--freq'])
         self.assertTrue(compare_to_expected('out.frq', 'example/locus8.frq'))
         self.addCleanup(os.remove, 'out.frq')
         self.addCleanup(os.remove, 'out.frq.log')
     
-    def test_calculate_het(self):    
-        vcf_scripts.calculate_heterozygosity(['--gzvcf', 'example/locus8.vcf.gz', '--out', 'out'])
+    def test_het (self):    
+        vcf_scripts.vcftools_wrapper(['--gzvcf', 'example/locus8.vcf.gz', '--out', 'out', '--het'])
         self.assertTrue(compare_to_expected('out.het', 'example/locus8.het'))
         self.addCleanup(os.remove, 'out.het')
         self.addCleanup(os.remove, 'out.het.log')
