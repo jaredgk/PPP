24d323d86adac1d44b18f2f514ca7435393c0bc4
Jared Knoblauch <jaredknoblauch@gmail.com>
2017-03-20 17:01:20 -0400

    adding log to vcf-seq and renamed logging functions

--- jared/logging_module.py
+++ jared/logging_module.py
@@ -3,15 +3,24 @@ import logging
 
 
 
-def formatLogger():
+def individualFunctionLogger():
     fmt_def = "%(asctime)s - %(name)s - %(levelname)s: %(message)s"
     fmtr = logging.Formatter(fmt=fmt_def)
     s_handler = logging.StreamHandler()
     s_handler.setFormatter(fmtr)
+    s_handler.setLevel(logging.WARNING)
+    f_handler = logging.FileHandler('root.log',mode="w")
+    f_handler.setFormatter(fmtr)
     rootlogger = logging.getLogger()
+    rootlogger.setLevel('INFO')
     rootlogger.addHandler(s_handler)
+    rootlogger.addHandler(f_handler)
+    def exp_handler(etype,val,tb):
+        logging.error("%s" % (val), exc_info=(etype,val,tb))
+    #exp_handler.append(sys.__excepthook__)
+    sys.excepthook = exp_handler
 
-def switchLogger(name):
+def pipeSwitchLogger(name):
     filename = name+'.log'
     fmt_def = "%(asctime)s - %(name)s - %(levelname)s: %(message)s"
     fmtr = logging.Formatter(fmt=fmt_def)
