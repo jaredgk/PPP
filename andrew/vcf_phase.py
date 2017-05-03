2f4781dc7d1944a83692dd95c7d7d60cc58a5faa
aewebb80 <tug41380@temple.edu>
2017-05-03 15:20:00 -0400

    Updated vcf_phase to work with both beagle and shapeit

--- andrew/vcf_phase.py
+++ andrew/vcf_phase.py
@@ -1,4 +1,12 @@
-import os, sys, subprocess, argparse, glob
+import os
+import sys
+import subprocess
+import argparse
+import glob
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.pardir,'jared')))
+
+import vcf_reader_func
 
 def phase_argument_parser(passed_arguments):
     '''Phase Argument Parser - Assigns arguments for vcftools from command line.
@@ -33,6 +41,7 @@ def phase_argument_parser(passed_arguments):
 
     # Other basic arguments. Expand as needed
     phase_parser.add_argument('--out', help = 'Defines the output filename', default = 'out', action = parser_confirm_no_file())
+    phase_parser.add_argument('--estimate-file', help = 'Defines the estimated genotype frequency filename. For used if using beagle', default = 'estimated_gt', action = parser_confirm_no_file())
 
     if passed_arguments:
         return phase_parser.parse_args(passed_arguments)
@@ -47,28 +56,84 @@ def possible_beagle_paths ():
     for current_path in possible_paths:
         print [n for n in glob.glob(current_path) if os.path.isfile(n)]
 
+def assign_vcf_extension (filename):
+    # Checks if the file is unzipped, bgzipped, or gzipped
+    vcfname_format = vcf_reader_func.checkFormat(filename)
+
+    if vcfname_format == 'nozip':
+        return '.vcf'
+    elif vcfname_format == 'gzip' or vcfname_format == 'bgzip':
+        return '.vcf.gz'
+    else:
+        sys.exit('Unknown file format')
+
 def run (passed_arguments = []):
     ''' Wrapper code for Phasing. Commands are assigned using argparse.'''
 
     # Grab VCF arguments from command line
     phase_args = phase_argument_parser(passed_arguments)
 
-    # Container for the phasing subprocess command
-    phase_call_args = ['out=' + phase_args.out]
+    # Assign file extension for VCF input file
+    vcfname_ext = assign_vcf_extension(phase_args.vcfname)
 
-    if phase_args.phase_algorithm:
+    # Used to confirm if the VCF input was renamed
+    vcfname_renamed = False
+
+    # Confirm input has correct file extension
+    if vcfname_ext not in phase_args.vcfname:
+        vcfname_renamed = True
+        os.rename(phase_args.vcfname, phase_args.vcfname + vcfname_ext)
+        phase_args.vcfname += vcfname_ext
+
+    if phase_args.phase_algorithm == 'beagle':
         # Assign the algorithm
-        phase_call_algorithm = ['java', '-jar', 'bin/beagle.jar']
+        algorithm_call_args = ['java', '-jar', 'bin/beagle.jar']
+
         # Assign the arguments for the algorithm
-        phase_call_args.append('gl=' + phase_args.vcfname)
+        likelihood_call_args = ['gtgl=' + phase_args.vcfname, 'out=' + phase_args.estimate_file]
 
-    # Assigns the vcftools arguments to a single command line
-    #beagle_command =  [split_arg for arg in vars(beagle_args) for split_arg in getattr(beagle_args, arg)]
+        # beagle estimated genotype frequency subprocess call
+        likelihood_call = subprocess.Popen(algorithm_call_args + likelihood_call_args, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        likelihood_out, likelihood_err = likelihood_call.communicate()
 
-    # vcftools subprocess call
-    phase_call = subprocess.Popen(phase_call_algorithm + phase_call_args, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-    phase_out, phase_err = phase_call.communicate()
-    print phase_out, phase_err
+        # Confirms that beagle finished without error
+        if likelihood_err:
+            sys.exit('Error creating the estimated genotype frequency file. Please check input file.')
+
+        # Assigns the arguments for phasing
+        phase_call_args = ['gt=' + phase_args.estimate_file + vcfname_ext, 'out=' + phase_args.out]
+
+        # Phasing subprocess call
+        phase_call = subprocess.Popen(algorithm_call_args + phase_call_args, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        phase_out, phase_err = phase_call.communicate()
+        print phase_out, phase_err
+
+    elif phase_args.phase_algorithm == 'shapeit':
+        # Assign the algorithm
+        algorithm_call_args = ['./bin/shapeit']
+
+        # Assigns the arguments for phasing
+        phase_call_args = ['--input-vcf', phase_args.vcfname, '-O', phase_args.out]
+
+        # Phasing subprocess call
+        phase_call = subprocess.Popen(algorithm_call_args + phase_call_args, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        phase_out, phase_err = phase_call.communicate()
+
+        # Confirms that shapeit finished without error
+        if phase_err:
+            sys.exit('Error occured in phasing. Please check input file.')
+
+        # Assigns the arguments for converting the phased output into a vcf file
+        convert_call = ['-convert', '--input-haps', phase_args.out, '--output-vcf', phase_args.out + vcfname_ext]
+
+        # Convert subprocess call
+        convert_call = subprocess.Popen(algorithm_call_args + convert_call, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        convert_out, convert_err = convert_call.communicate()
+        print convert_out, convert_err
+
+    # Reverts the VCF input file
+    if vcfname_renamed:
+        os.rename(phase_args.vcfname, phase_args.vcfname[:-len(vcfname_ext)])
 
 if __name__ == "__main__":
     run()
