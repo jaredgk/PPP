0055d76b8d71044d7adf6374283ac2ff495ee5c0
aewebb80 <tug41380@temple.edu>
2017-10-03 22:33:38 -0400

    Updated vcf_phase due to loss of current version

--- andrew/vcf_phase.py
+++ andrew/vcf_phase.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import subprocess
+import shutil
 import argparse
 import glob
 import logging
@@ -23,15 +24,6 @@ def phase_argument_parser(passed_arguments):
                 setattr(args, self.dest, value)
         return customAction
 
-    def parser_confirm_no_file ():
-        '''Custom action to confirm file does not exist'''
-        class customAction(argparse.Action):
-            def __call__(self, parser, args, value, option_string=None):
-                if os.path.isfile(value):
-                    raise IOError # File found
-                setattr(args, self.dest, value)
-        return customAction
-
     def metavar_list (var_list):
         '''Create a formmated metavar list for the help output'''
         return '{' + ', '.join(var_list) + '}'
@@ -46,7 +38,8 @@ def phase_argument_parser(passed_arguments):
     phase_parser.add_argument('--phase-algorithm', metavar = metavar_list(phasing_list), help = 'Specifies the phase algorithm to be used', type = str, choices = phasing_list, default = phasing_default)
 
     # Other basic arguments. Expand as needed
-    phase_parser.add_argument('--out', help = 'Defines the output filename', default = 'out', action = parser_confirm_no_file())
+    phase_parser.add_argument('--out', help = 'Defines the output filename')
+    phase_parser.add_argument('--out-prefix', help = 'Defines the output prefix (used by phasing algorithms)', default = 'out')
 
     if passed_arguments:
         return phase_parser.parse_args(passed_arguments)
@@ -59,14 +52,6 @@ def logArgs(args, pipeline_function):
     for k in vars(args):
         logging.info('Arguments %s: %s' % (k, vars(args)[k]))
 
-def possible_beagle_paths ():
-    possible_paths = ['beagle*.jar']
-    if os.name == 'posix':
-        possible_paths.extend(['\usr\local\bin\beagle*.jar', '\usr\bin\beagle*.jar'])
-
-    for current_path in possible_paths:
-        print [n for n in glob.glob(current_path) if os.path.isfile(n)]
-
 def assign_vcf_extension (filename):
     # Checks if the file is unzipped, bgzipped, or gzipped
     vcfname_format = vcf_reader_func.checkFormat(filename)
@@ -77,6 +62,74 @@ def assign_vcf_extension (filename):
         return '.vcf.gz'
     else:
         sys.exit('Unknown file format')
+
+def check_beagle_for_errors (beagle_stdout):
+    '''
+        Checks the beagle stdout for errors
+
+        Parameters
+        ----------
+        beagle_stdout : str
+            beagle stdout
+
+        Raises
+        ------
+        IOError
+            If beagle stdout returns an error
+    '''
+
+    # Returns True if the job completed without error
+    if 'End time:' in str(beagle_stdout) and 'finished'  in str(beagle_stdout):
+        pass
+
+    # Print output for beagle if error is detected
+    elif 'ERROR:' in str(beagle_stdout):
+        # Splits log into list of lines
+        beagle_stdout_lines = beagle_stdout.splitlines()
+        # Prints the error(s)
+        logging.error('\n'.join((output_line for output_line in beagle_stdout_lines if output_line.startswith('ERROR:'))))
+        raise Exception('\n'.join((output_line for output_line in beagle_stdout_lines if output_line.startswith('ERROR:'))))
+
+    # Print output if not completed and no error found. Unlikely to be used, but included.
+    else:
+        logging.error(beagle_stdout)
+        raise Exception(beagle_stdout)
+
+def check_shapeit_for_errors (shapeit_stdout):
+    '''
+        Checks the shapeit stdout for errors
+
+        Parameters
+        ----------
+        shapeit_stdout : str
+            shapeit stdout
+
+        Raises
+        ------
+        IOError
+            If shapeit stdout returns an error
+    '''
+
+    # Returns True if the job completed without error
+    if 'Running time:' in str(shapeit_stdout):
+        pass
+
+    # Print output if not completed and no error found. Unlikely to be used, but included.
+    else:
+        logging.error(shapeit_stdout)
+        raise Exception(shapeit_stdout)
+
+def combine_shapeit_logs (shapeit_logs, new_log_filename):
+    # Combine log files
+    with open(new_log_filename, 'wb') as out_file:
+        for shapeit_log in shapeit_logs:
+            if not os.path.isfile(shapeit_log):
+                raise IOError # File not found
+            with open(shapeit_log, 'rb') as in_file:
+                shutil.copyfileobj(in_file, out_file)
+    # Remove old log files
+    for shapeit_log in shapeit_logs:
+        os.remove(shapeit_log)
 
 def run (passed_arguments = []):
     '''
@@ -134,23 +187,33 @@ def run (passed_arguments = []):
         algorithm_call_args = ['java', '-jar', 'bin/beagle.jar']
 
         # Assigns the arguments for phasing
-        phase_call_args = ['gt=' + phase_args.vcfname, 'out=' + phase_args.out]
+        phase_call_args = ['gt=' + phase_args.vcfname, 'out=' + phase_args.out_prefix]
 
         logging.info('beagle phasing parameters assigned')
 
         # Phasing subprocess call
         phase_call = subprocess.Popen(algorithm_call_args + phase_call_args, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
         phase_out, phase_err = phase_call.communicate()
-        print phase_out, phase_err
+
+        # Check beagle call for errors
+        check_beagle_for_errors(phase_out)
 
         logging.info('beagle phasing complete')
+
+        if phase_args.out:
+            shutil.move(phase_args.out_prefix + vcfname_ext, phase_args.out)
+            shutil.move(phase_args.out_prefix + '.log', phase_args.out + '.log')
+        else:
+            shutil.move(phase_args.out_prefix + '.log', phase_args.out_prefix + vcfname_ext + '.log')
+
+        logging.info('beagle log file created')
 
     elif phase_args.phase_algorithm == 'shapeit':
         # Assign the algorithm
         algorithm_call_args = ['shapeit']
 
         # Assigns the arguments for phasing
-        phase_call_args = ['--input-vcf', phase_args.vcfname, '-O', phase_args.out]
+        phase_call_args = ['--input-vcf', phase_args.vcfname, '-O', phase_args.out_prefix, '-L', phase_args.out_prefix + '.p.log']
 
         logging.info('shapeit phasing parameters assigned')
 
@@ -158,25 +221,40 @@ def run (passed_arguments = []):
         phase_call = subprocess.Popen(algorithm_call_args + phase_call_args, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
         phase_out, phase_err = phase_call.communicate()
 
-        # Confirms that shapeit finished without error
-        if phase_err:
-            logging.error('Error occured in phasing. Please check input file.')
-            sys.exit('Error occured in phasing. Please check input file.')
+        # Check shapeit call for errors
+        check_shapeit_for_errors(phase_out)
 
         logging.info('shapeit phasing complete (non-VCF format)')
 
         # Assigns the arguments for converting the phased output into a vcf file
-        convert_call = ['-convert', '--input-haps', phase_args.out, '--output-vcf', phase_args.out + vcfname_ext]
+        convert_call = ['-convert', '--input-haps', phase_args.out_prefix, '--output-vcf', phase_args.out_prefix + vcfname_ext, '-L', phase_args.out_prefix + '.c.log']
 
         logging.info('shapeit conversion parameter assigned')
 
         # Convert subprocess call
         convert_call = subprocess.Popen(algorithm_call_args + convert_call, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
         convert_out, convert_err = convert_call.communicate()
-        print convert_out, convert_err
 
-        logging.info('shapeit conversion complete (non-VCF to VCF)')
+        # Check shapeit call for errors
+        check_shapeit_for_errors(convert_out)
 
+        logging.info('shapeit conversion to VCF complete')
+
+        if phase_args.out:
+            shutil.move(phase_args.out_prefix + vcfname_ext, phase_args.out)
+            combine_shapeit_logs([phase_args.out_prefix + '.p.log', phase_args.out_prefix + '.c.log'],  phase_args.out + '.log')
+        else:
+            combine_shapeit_logs([phase_args.out_prefix + '.p.log', phase_args.out_prefix + '.c.log'],  phase_args.out_prefix + vcfname_ext + '.log')
+
+        logging.info('shapeit log file created')
+
+        # Add options for not removing
+        os.remove('out.haps')
+        os.remove('out.sample')
+        os.remove('out.p.ind.mm')
+        os.remove('out.p.snp.mm')
+
+        logging.info('shapeit intermediate file removed')
 
     # Reverts the VCF input file
     if vcfname_renamed:
