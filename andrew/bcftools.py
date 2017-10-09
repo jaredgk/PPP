ae2092679834a1198ee9d1466bc8e48cfa548e3e
aewebb80 <tug41380@temple.edu>
2017-10-09 16:30:20 -0400

    Changes for phaser on galaxy

--- andrew/bcftools.py
+++ andrew/bcftools.py
@@ -5,7 +5,7 @@ import subprocess
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.pardir,'jared')))
 
-import vcf_reader_func
+from vcf_reader_func import checkFormat
 
 def call_bcftools (bcftools_call_args):
 
@@ -17,7 +17,50 @@ def call_bcftools (bcftools_call_args):
 
     logging.info('bcftools call complete')
 
-    return bcftools_err
+    return bcftools_out, bcftools_err
+
+def check_for_index (filename):
+
+    # Assign the file format
+    file_format = checkFormat(filename)
+
+    # Check if the file to be indexed is a vcf.gz
+    if file_format == 'bgzip':
+        # Check if the index (.tbi) exists
+        if os.path.isfile(filename + '.tbi'):
+            return True
+
+    # Check if the file to be indexed is a bcf
+    elif file_format == 'bcf':
+        # Check if the index (.csi) exists
+        if os.path.isfile(filename + '.csi'):
+            return True
+
+    # Return false if no index is found
+    return False
+
+def create_index (filename):
+
+    # Assign the file format
+    file_format = checkFormat(filename)
+
+    # Check if the file to be indexed is a vcf.gz
+    if file_format == 'bgzip':
+        # Create a index (.tbi)
+        index_out, index_err = call_bcftools(['index', '-t', filename])
+
+    # Check if the file to be indexed is a bcf
+    elif file_format == 'bcf':
+        # Create a index (.csi)
+        index_out, index_err = call_bcftools(['index', '-c', filename])
+
+    # Report if file cannot be indexed
+    else:
+        raise Exception('Error creating index for: %s. Only .bcf and .vcf.gz (bgzip) files are supported.' % filename)
+
+    # Check bcftools for errors
+    if index_out or index_err:
+        print (index_out, index_err)
 
 def convert_to_bcf (filename):
     pass
