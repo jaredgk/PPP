e79ff66f8ca6375180589190444b6ce70689f7e4
Jared Knoblauch <jaredknoblauch@gmail.com>
2018-04-06 15:14:37 -0400

    changing to zero flags

--- jared/argparse_sets.py
+++ jared/argparse_sets.py
@@ -4,9 +4,9 @@ import argparse
 def addRegionArgs(parser):
     parser.add_argument('--regionf', dest="region_name",
                         help="Name of gene region file")
-    parser.add_argument('--gr1', dest="region_idx", action="store_true",
+    parser.add_argument('--zeroho', dest="zeroho", action="store_true",
                         help="Region list is 1 indexed, not 0")
-    parser.add_argument('--halfopen', dest="halfopen", action="store_false",
+    parser.add_argument('--zeroclosed', dest="zeroclosed", action="store_true",
                         help="If set, use closed coordinates instead of half-open")
     parser.add_argument('--regcol', dest='colstr', help= (
                         "Comma-separated list of columns for gene region "
