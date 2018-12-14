1cd773cfe3d4db59701394ed9fa232da3b1e03b4
Jared Knoblauch <jaredknoblauch@gmail.com>
2018-12-14 15:48:02 -0500

    adding pysam functionality and ind subsampling

--- jared/get_nonmissing_chunks.py
+++ jared/get_nonmissing_chunks.py
@@ -2,6 +2,8 @@ import sys
 import argparse
 import gzip
 import io
+from vcf_reader_func import VcfReader
+from model import Model, read_model_file
 
 def createParser():
     parser = argparse.ArgumentParser()
@@ -23,6 +25,8 @@ def createParser():
                         help=("Add 'chr' to start of chromosome name"))
     parser.add_argument('--removechr',dest="removechr",action="store_true",
                         help=("Remove 'chr' from start of chromosome name"))
+    parser.add_argument('--pop',dest="popname",help="Model file if only using subset of inds in a file")
+    parser.add_argument("--tbi", dest="tabix_index", help="Path to bgzipped file's index if name doesn't match VCF file")
     return parser
 
 def getl(f,compressed=False):
@@ -47,20 +51,36 @@ def outputLine(chrom,start_pos,end_pos,args):
         sp -= 1
         ep -= 1
     chrom = fixChromName(chrom,args.addchr,args.removechr)
-    return chrom+'\t'+str(start_pos)+'\t'+str(end_pos)+'\n'
+    return chrom+'\t'+str(sp)+'\t'+str(ep)+'\n'
 
 def regionsWithData(sysargs):
     parser = createParser()
     args = parser.parse_args(sysargs)
     compressed_input = False
+    #if args.vcfname is None:
+    #    instream = sys.stdin
+    #elif args.vcfname[-3:] == '.gz':
+    #    compressed_input = True
+    #    instream = gzip.open(args.vcfname,'r')
+   # else:
+   #     instream = open(args.vcfname,'r')
+
+
     if args.vcfname is None:
-        instream = sys.stdin
-    elif args.vcfname[-3:] == '.gz':
-        #instream = io.TextIOWrapper(io.BufferedReader(gzip.open(args.vcfname),'rb'),encoding="utf-8")
-        compressed_input = True
-        instream = gzip.open(args.vcfname,'r')
+        vcfname = '-'
     else:
-        instream = open(args.vcfname,'r')
+        vcfname = args.vcfname
+
+    popmodel = None
+    if args.popname is not None:
+        popmodels = read_model_file(args.modelname)
+        pp = list(popmodels.keys())
+        popmodel = popmodels[pp[0]]
+
+    vcf_in = VcfReader(vcfname,
+                       popmodel=popmodel,
+                       index=args.tabix_index)
+
     if args.outname is not None:
         outstream = open(args.outname,'w')
     else:
@@ -79,31 +99,37 @@ def regionsWithData(sysargs):
     prev_chrom = ''
 
     in_section = False
-    line = getl(instream,compressed_input)
-    while len(line.strip()) > 0:
+    #line = getl(instream,compressed_input)
+    #while len(line.strip()) > 0:
     #for line in instream:
-        if line[0] == '#':
-            line = getl(instream,compressed_input)
-            continue
-        la = line.strip().split()
+    for record in vcf_in.reader:
+        #if line[0] == '#':
+        #    line = getl(instream,compressed_input)
+        #    continue
+        #la = line.strip().split()
         missing_for_site = False
         missing_at_site = 0
-        for i in range(9,len(la)):
-            geno = la[i]
-            if '.' in geno:
+        #for i in range(9,len(la)):
+        for i in range(len(record.samples)):
+            #geno = la[i]
+            #if '.' in geno:
+            if record.samples[i].alleles[0] in [None,'N']:
                 missing_at_site += 1
                 if missing_at_site > args.missing_count:
                     missing_for_site = True
                     break
-        cur_pos = int(la[1])
-        if prev_pos == 0 or prev_chrom != la[0]:
+        #cur_pos = int(la[1])
+        cur_pos = record.pos
+        #if prev_pos == 0 or prev_chrom != la[0]:
+        if prev_pos == 0 or prev_chrom != record.chrom:
             if prev_pos != 0:
                 start_pos = ((prev_miss_pos+prev_full_pos+1)//2 if args.extend else prev_full_pos)
                 end_pos = prev_pos
                 outstream.write(outputLine(prev_chrom,start_pos,end_pos,args))
             prev_miss_pos = cur_pos
             prev_full_pos = cur_pos
-            prev_chrom = la[0]
+            #prev_chrom = la[0]
+            prev_chrom = record.chrom
             if not missing_for_site:
                 in_section = True
             prev_pos = cur_pos
@@ -126,7 +152,7 @@ def regionsWithData(sysargs):
                     #    end_pos -= 1
                     #chrom = fixChromName(la[0],args.addchr,args.removechr)
                     #outstream.write(chrom+'\t'+str(start_pos)+'\t'+str(end_pos)+'\n')
-                    outstream.write(outputLine(la[0],start_pos,end_pos,args))
+                    outstream.write(outputLine(record.chrom,start_pos,end_pos,args))
                     #sys.stdout.write(la[0]+'\t'+str(prev_full_pos)+'\t'+str(prev_pos)+'\n')
                     #print (la[0],prev_miss_pos,prev_full_pos,prev_pos,cur_pos,diff)
         else:
@@ -135,7 +161,7 @@ def regionsWithData(sysargs):
                 prev_full_pos = cur_pos
                 in_section = True
         prev_pos = cur_pos
-        line = getl(instream,compressed_input)
+        #line = getl(instream,compressed_input)
     if in_section:
         if args.extend:
             start_pos = (prev_miss_pos+prev_full_pos+1)//2
