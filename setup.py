bb3c0e7f31c215919ba5a97e50c60e027681fcb6
Jared Knoblauch <jaredknoblauch@gmail.com>
2019-02-26 15:29:56 -0500

    changing setup scripts

--- setup.py
+++ setup.py
@@ -5,4 +5,7 @@ from setuptools import setup
 setup(name="ppp",
       version="0.0.1",
       description="first setup file",
-      packages=['pppipe'])
+      include_package_data=True,
+      packages=['pgpipe'])
+
+#packages=setuptools.find_packages() to automate package finding?
