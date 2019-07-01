9b837c157f078746da25a04b4dc3ba6d930d5a15
aewebb80 <tug41380@temple.edu>
2019-07-01 14:45:22 -0400

    travis-ci R install attempt

--- install_R.sh
+++ install_R.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 set -ex
+sudo apt-get install gfortran
 wget http://cran.rstudio.com/src/base/R-3/R-3.4.1.tar.gz
-tar xvf R-3.4.1.tar.gz
-cd R-3.4.1 && ./configure --prefix=/usr/R --silent && make && sudo make install && cd ..
+tar -xf R-3.4.1.tar.gz
+cd R-3.4.1 && ./configure --prefix=/usr/R --enable-R-shlib --silent && make --silent && sudo make install && cd ..
+export PATH="/usr/R/bin:$PATH"
+export RSTUDIO_WHICH_R="/usr/R/lib/R/bin/R"
+whereis R
+
