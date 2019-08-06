0c62fcb7fd1e261e29209ed72cff3ef9d15ec57b
aewebb80 <tug41380@temple.edu>
2019-08-06 13:08:59 -0400

    travis-ci R install attempt

--- install_R.sh
+++ install_R.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -ex
-sudo apt-get update
-sudo apt-get install gfortran
-wget http://cran.rstudio.com/src/base/R-3/R-3.4.1.tar.gz
-tar -xf R-3.4.1.tar.gz
-cd R-3.4.1 && ./configure --prefix=/usr/R --enable-R-shlib --silent && make --silent && sudo make install && cd ..
-export PATH=/usr/R/bin:$PATH
-export R_HOME=/usr/R/lib64/R
-whereis R
-
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda info -a
+conda create -q -n test-env python=$TRAVIS_PYTHON_VERSION
+source activate test-env
