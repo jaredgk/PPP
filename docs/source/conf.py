686785195322bf3e5290853d2d01c4db78c31beb
aewebb80 <tug41380@temple.edu>
2019-08-07 15:19:59 -0400

    Trying Mock without cython

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['pysam', 'rpy2', 'cython', 'tzlocal']
+MOCK_MODULES = ['pysam', 'rpy2', 'tzlocal']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
