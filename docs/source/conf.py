1f0fde51679fbd60143800e717624f1b1f464d80
Andrew Webb <tug41380@temple.edu>
2019-08-07 16:05:15 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['pysam', 'rpy2', 'tzlocal']
+MOCK_MODULES = ['pysam', 'rpy2', 'tzlocal', 'rpy2.robjects']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
