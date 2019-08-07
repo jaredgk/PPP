c2d69e9698345878e1490032ccff83031877120e
Andrew Webb <tug41380@temple.edu>
2019-08-06 23:25:54 -0400

    Update install_executables.sh

--- install_executables.sh
+++ install_executables.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 set -ex
-wget https://github.com/vcftools/vcftools/tarball/master -O master_vcftools.tar.gz
-tar -xzf master_vcftools.tar.gz
-cd vcftools* && ./autogen.sh --silent && ./configure --prefix=/usr --silent && make --silent && sudo make install && cd ..
 wget http://faculty.washington.edu/browning/beagle/beagle.21Jan17.6cc.jar
 mv beagle.21Jan17.6cc.jar beagle.jar
 pip install -e ./
