486be99cc12268484ee26db6754c07d668e65ad1
Andrew Webb <tug41380@temple.edu>
2019-08-06 22:59:20 -0400

    Moving bcftools install

--- install_executables.sh
+++ install_executables.sh
@@ -5,10 +5,3 @@ tar -xzf master_vcftools.tar.gz
 cd vcftools* && ./autogen.sh --silent && ./configure --prefix=/usr --silent && make --silent && sudo make install && cd ..
 wget http://faculty.washington.edu/browning/beagle/beagle.21Jan17.6cc.jar
 mv beagle.21Jan17.6cc.jar beagle.jar
-wget https://github.com/samtools/htslib/releases/download/1.6/htslib-1.6.tar.bz2 -O master_htslib.tar.bz2
-tar -xjf master_htslib.tar.bz2
-cd htslib* && ./configure --prefix=/usr --silent && make --silent && sudo make install && cd ..
-wget https://github.com/samtools/bcftools/releases/download/1.9/bcftools-1.9.tar.bz2 -O master_bcftools.tar.bz2
-tar -xjf master_bcftools.tar.bz2
-cd bcftools* && ./configure --prefix=/usr --silent && make --silent && sudo make install && cd ../
-pip install -e ./
