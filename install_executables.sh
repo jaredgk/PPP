f4bfc073b7e665c5626086a9bb941d188faab326
Andrew Webb <tug41380@temple.edu>
2019-09-04 15:08:25 -0400

    Update install_executables.sh

--- install_executables.sh
+++ install_executables.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-wget http://faculty.washington.edu/browning/beagle/beagle.21Jan17.6cc.jar
-mv beagle.21Jan17.6cc.jar beagle.jar
+wget https://faculty.washington.edu/browning/beagle/beagle.24Aug19.3e8.jar
+mv beagle.24Aug19.3e8.jar beagle.jar
 pip install -e ./
