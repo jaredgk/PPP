5c036e4e92d454e68a364eb59cfa242d0052a998
aewebb80 <tug41380@temple.edu>
2019-10-20 00:11:57 -0400

    removing mkdir

--- install_executables.sh
+++ install_executables.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -ex
-mkdir $TRAVIS_BUILD_DIR/bin
 export PATH=$PATH:$TRAVIS_BUILD_DIR/bin/
 wget https://faculty.washington.edu/browning/beagle/beagle.24Aug19.3e8.jar
 mv beagle.24Aug19.3e8.jar $TRAVIS_BUILD_DIR/bin/beagle.jar
