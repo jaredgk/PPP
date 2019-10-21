2f91188f64bdac062e491aba07a8baea0e0f5411
aewebb80 <tug41380@temple.edu>
2019-10-21 18:44:50 -0400

    updated plink2.0 link

--- install_executables.sh
+++ install_executables.sh
@@ -3,6 +3,6 @@ set -ex
 export PATH=$PATH:$TRAVIS_BUILD_DIR/bin/
 wget https://faculty.washington.edu/browning/beagle/beagle.24Aug19.3e8.jar
 mv beagle.24Aug19.3e8.jar $TRAVIS_BUILD_DIR/bin/beagle.jar
-wget https://s3.amazonaws.com/plink2-assets/plink2_linux_x86_64_20190826.zip -O plink2.zip && unzip plink2.zip
+wget https://s3.amazonaws.com/plink2-assets/plink2_linux_x86_64_20191020.zip -O plink2.zip && unzip plink2.zip
 mv plink2 $TRAVIS_BUILD_DIR/bin
 pip install -e ./
