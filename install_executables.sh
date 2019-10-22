3da63bd4190bb8edb29ad9324bbd75d7febb17cd
Andrew Webb <tug41380@temple.edu>
2019-10-21 20:45:15 -0400

    Update install_executables.sh

--- install_executables.sh
+++ install_executables.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 wget https://faculty.washington.edu/browning/beagle/beagle.24Aug19.3e8.jar
-mv beagle.24Aug19.3e8.jar /usr/local/bin/beagle.jar
+sudo mv beagle.24Aug19.3e8.jar /usr/local/bin/beagle.jar
 wget https://s3.amazonaws.com/plink2-assets/plink2_linux_x86_64_20191020.zip -O plink2.zip && unzip plink2.zip
-mv plink2 /usr/local/bin
+sudo mv plink2 /usr/local/bin
 pip install -e ./
