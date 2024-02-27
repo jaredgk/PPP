c7511a578e858b136ec9838e95b699d978e402dd
Raya Esplin <Raya Esplin>
2024-02-27 19:21:22 +0000

    sets up a file for pixy

---
+++ pgpipe/pixy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+    Automates estimating nucleotide diversity within (π) and between (dxy) 
+    populations from a VCF using pixy
+
+    ##################
+    Command-line Usage
+    ##################
+    The pixy statistic calculator may be called using the following command:
+
+     .. code-block:: bash
+        
+        pixy.py
+"""
