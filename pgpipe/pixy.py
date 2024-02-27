b61fa4c8ec31de53a50f19dec8fad84d05a01888
Raya Esplin <Raya Esplin>
2024-02-27 23:42:05 +0000

    adds arguments to the pixy call args

--- pgpipe/pixy.py
+++ pgpipe/pixy.py
@@ -19,7 +19,7 @@ from pgpipe.logging_module import initLogger, logArgs
 from pgpipe.misc import argprase_kwargs
 
 
-def pixy_parser(passed_arguments=[]):
+def pixy_calc_parser(passed_arguments=[]):
     """
     Pixy Argument Parser
 
@@ -29,7 +29,7 @@ def pixy_parser(passed_arguments=[]):
     ----------
     passed_arguments : list, optional
         Parameters passed by another function. sys.argv is used if
-        not given. 
+        not given.
 
     Raises
     ------
@@ -91,7 +91,7 @@ def pixy_parser(passed_arguments=[]):
 
     # Required arguments
     pixy_parser.add_argument(
-        "-vcf",
+        "--vcf",
         help="Defines the path and/or filename of the VCF",
         type=str,
         required=True,
@@ -122,7 +122,7 @@ def pixy_parser(passed_arguments=[]):
         required=False,
     )
     pixy_parser.add_argument(
-        "--bed_file",
+        "--bed-file",
         type=str,
         nargs="?",
         help="Path to a headerless .BED file with columns [chrom chromStart chromEnd]",
@@ -134,7 +134,6 @@ def pixy_parser(passed_arguments=[]):
         "--out-dir",
         type=str,
         nargs="?",
-        default="",
         help="Defines the output directory",
         required=False,
     )
@@ -142,7 +141,6 @@ def pixy_parser(passed_arguments=[]):
         "--out",
         type=str,
         nargs="?",
-        default="pixy",
         help="Defines prefix for output file(s)",
         required=False,
     )
@@ -151,4 +149,85 @@ def pixy_parser(passed_arguments=[]):
         return vars(pixy_parser.parse_args(passed_arguments))
     else:
         return vars(pixy_parser.parse_args())
+
+
+def run(**kwargs):
+    """
+    Statistic Calculations using Pixy.
+
+    This function uses the argparse-based function :py:func:`pixy_parser`
+    to parse either sys.argv or passed_arguments to obtain the parameters below.
+    The parameters are then translated to their pixy equivalent. Once all the
+    parameters are assigned, pixy is called.
+
+    Parameters
+    ----------
+    --vcf : str
+        Input VCF filename and/or path
+    --pop-file : str
+        Input populations filename and/or path
+    --stats : str
+        Specifies statistic(s) to calculate
+    --statistic-window-size : int
+        Specifices window size
+    --bed-file : str
+        Path to a .BED file
+    --out-dir : str
+        Output directory
+    --out  : str
+        Output prefix
+
+    Raises
+    ------
+    IOError
+        Output file already exists and --overwrite is not specified
+    Exception
+        Incompatible arguments
+    """
+    # Update kwargs with defaults
+    if __name__ != "__main__":
+        kwargs = argprase_kwargs(kwargs, pixy_calc_parser)
+
+    # Assign arguments
+    pixy_args = argparse.Namespace(**kwargs)
+
+    # Adds arguments to the log file
+    logArgs(pixy_args, func_name="pixy")
+
+    # Argument container for pixy
+    pixy_call_args = []
+
+    # TODO add args to pixy_call_args
+    # Add required arguments
+    if pixy_args.vcf:
+        pixy_call_args.extend(["--vcf" + pixy_args.vcf])
+
+    if pixy_args.pop_file:
+        pixy_call_args.extend(["--populations" + pixy_args.pop_file])
+
+    if pixy_args.stats:
+        pixy_call_args.extend(["stats" + {arg} for arg in pixy_args.stats])
+
+    # Assign one of either
+    if pixy_args.bed_file and pixy_args.statistic_window_size:
+        raise Exception(
+            "Cannot have both bed-file and statistic-window-size parameters. Must be one or the other"
+        )
+    
+    elif not pixy_args.bed_file and not pixy_args.statistic_window_size:
+        raise Exception("Must specificy a bed file or a window size")
+    
+    if pixy_args.bed_file and not pixy_args.statistic_window_size:
+        pixy_call_args.extend(["--bed_file" + pixy_args.bed_file])
+
+    elif pixy_args.statistic_window_size and not pixy_args.bed_file:
+        pixy_call_args.extend(["--window_size" + str(pixy_args.statistic_window_size)])
+
+    # Add optional arguments
+    if pixy_args.out:
+        pixy_call_args.extend(["--output_prefix" + pixy_args.out])
+    
+    if pixy_args.out_dir:
+        pixy_call_args.extend(["--output_folder" + pixy_args.out_dir])
+
     
